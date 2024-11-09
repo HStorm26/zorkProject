9aa81aa6b5488a5ab1b73b73407a90b063b72071
hzeigler <hzeigler@mail.umw.edu>
2024-11-08 21:22:35 -0500

    Flavor changes to HealthCommand.

--- src/HealthCommand.java
+++ src/HealthCommand.java
@@ -14,8 +14,8 @@ class HealthCommand extends Command{
         else if(hp >= 40){
             return "Your breath catches a little with each inhale. A bone or two doesn't feel quite right.\n";
         }
-        else if(hp >= 10){
-            return "Your heartbeat rings in your ears and it feels like there is more blood on you than in you.\n";
+        else if(hp >= 20){
+            return "Your heartbeat rings in your ears. Each pound shoots more pain into your head. Everything hurts.\n";
         }
         else{
             return "Your vision is dark around the edges. A feeling of doom has engulfed your thoughts.\n";
